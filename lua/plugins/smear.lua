305a89028082206c881d95461e7c07d4ec1fc88b
Edisow <edison.wica@gmail.com>
2026-01-30 09:13:32 -0500

    cursos smear

---
+++ lua/plugins/smear.lua
@@ -0,0 +1,9 @@
+return {
+	"sphamba/smear-cursor.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- Aquí puedes personalizar parámetros si quieres, por ejemplo:
+		-- cursor_color = "#ff00ff",
+		-- stiffness = 0.8,
+	},
+}
